b4691287670f963d790f65bc59f1c4c2772ae467
Muga-ai <lukemuga1@gmail.com>
2026-03-24 17:56:23 +0300

    fix: hardcode cloudinary cloud name

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: "dmur5e7nh",
   },
   images: {
     remotePatterns: [
